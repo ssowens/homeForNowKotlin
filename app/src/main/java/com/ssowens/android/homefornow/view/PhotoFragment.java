e308ca134093802e25adef50a9971f9f4185cdc0
ssowens <ssowens711@gmail.com>
2018-07-22 13:47:01 -0400

    Adding photofragment

---
+++ app/src/main/java/com/ssowens/android/homefornow/view/PhotoFragment.java
@@ -0,0 +1,31 @@
+package com.ssowens.android.homefornow.view;
+
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class PhotoFragment extends Fragment {
+
+    public static PhotoFragment newInstance() {
+        PhotoFragment fragment = new PhotoFragment();
+        fragment.setRetainInstance(true);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
