6b2dc98d768da1ba5c381e6998f2b72cb364ddaf
Sheila Owens <ssowens711@gmail.com>
2018-09-02 01:16:17 -0400

    Added widget code

---
+++ app/src/main/java/com/ssowens/android/homefornow/HomeForNowWidget.java
@@ -0,0 +1,84 @@
+package com.ssowens.android.homefornow;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.widget.RemoteViews;
+
+import java.text.DateFormat;
+import java.util.Date;
+
+/**
+ * Implementation of App Widget functionality.
+ */
+public class HomeForNowWidget extends AppWidgetProvider {
+
+    private static final String mSharedPrefFile =
+            "com.example.android.appwidgetsample";
+    private static final String COUNT_KEY = "count";
+
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                                int appWidgetId) {
+
+        SharedPreferences prefs = context.getSharedPreferences(
+                mSharedPrefFile, 0);
+        int count = prefs.getInt(COUNT_KEY + appWidgetId, 0);
+        count++;
+
+        String dateString =
+                DateFormat.getTimeInstance(DateFormat.SHORT).format(new Date());
+
+
+
+        // Construct the RemoteViews object
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.home_for_now_widget);
+        views.setTextViewText(R.id.appwidget_id, String.valueOf(appWidgetId));
+
+        views.setTextViewText(R.id.appwidget_update,
+                context.getResources().getString(
+                        R.string.data_count_format, count, dateString));
+
+        SharedPreferences.Editor prefEditor = prefs.edit();
+        prefEditor.putInt(COUNT_KEY + appWidgetId, count);
+        prefEditor.apply();
+
+        Intent intentUpdate = new Intent(context, HomeForNowWidget.class);
+        intentUpdate.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+
+        int[] idArray = new int[]{appWidgetId};
+        intentUpdate.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, idArray);
+
+        PendingIntent pendingUpdate = PendingIntent.getBroadcast(
+                context, appWidgetId, intentUpdate,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+
+        views.setOnClickPendingIntent(R.id.button_update, pendingUpdate);
+
+
+
+        // Instruct the widget manager to update the widget
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // There may be multiple widgets active, so update all of them
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+}
+
